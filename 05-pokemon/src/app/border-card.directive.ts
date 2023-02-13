58521fa6e03d5939c28da6b88da702600ae16dbe
GAudrey <gilmantaudrey.ag@gmail.com>
2023-02-13 14:45:56 +0100

    Angular Pokémon - add new pipe for pokémon color type

--- 05-pokemon/src/app/border-card.directive.ts
+++ 05-pokemon/src/app/border-card.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 export class BorderCardDirective {
   private initialColor: string = '#f5f5f5';
   private defaultColor: string = '#7fff00';
-  private defaultHeight: number = 125;
+  private defaultHeight: number = 175;
 
   constructor(private el: ElementRef) {
     this.setHeight(this.defaultHeight);
